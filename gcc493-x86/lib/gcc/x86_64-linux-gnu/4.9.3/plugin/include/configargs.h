0c3f79958f04183f7be16d8b22670ee837e869b8
chenc2 <chen.a.chen@intel.com>
2017-12-05 15:39:12 +0800

    Tools for edk2 gcc building in NT32.

---
+++ gcc493-x86/lib/gcc/x86_64-linux-gnu/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.3/configure --prefix=/gcc/xgcc --libexecdir=/gcc/xgcc/lib --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-linux-gnu --disable-werror --disable-shared --disable-libssp --disable-bootstrap --disable-nls --disable-libquadmath --without-headers --enable-lto --enable-languages=c --with-gmp=/gcc/xgcc --with-mpfr=/gcc/xgcc --with-mpc=/gcc/xgcc --with-libelf=/gcc/xgcc --with-pkgversion='EDK2 version 1.0-lto' --enable-plugin --enable-plugins --enable-multiarch --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --enable-twoprocess --disable-threads --disable-decimal-float --disable-win32-registry --disable-libc --with-windres";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "m64" }, { "cpu", "generic" }, { "arch_32", "i686" }, { "arch_64", "x86-64" } };
